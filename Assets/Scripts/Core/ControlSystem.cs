244cca0a002373ebee4adf7d3bc9ebf755eb4ece
HelloImZomi <zomizx@hotmail.com>
2019-05-07 21:41:54 -0700

    Unity Project

---
+++ Assets/Scripts/Core/ControlSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.ControlSystem
+{
+    public class ControlSystem
+    {
+        public static void Move(Transform rover,Transform target,float moveSpeed)
+        {
+            if (target)
+            {
+                rover.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+                rover.LookAt(target);
+            }            
+        }
+
+        public static void Rotate(Transform rover,float rotateSpeed,float yRotation)
+        {
+            Quaternion finalRotation = Quaternion.Euler(0, yRotation, 0);
+            rover.rotation = Quaternion.Lerp(rover.rotation, finalRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Rover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.ControlSystem;
+
+public class Rover : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 50f;
+
+    [SerializeField]
+    float rotateSpeed = 5f;
+
+    [SerializeField]
+    float yRotate = 0f;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float minDistance = 2f;
+
+    [SerializeField]
+    Vector2 coordinates;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+    public float YRotate { get => yRotate; set => yRotate = value; }
+    public float RotateSpeed { get => rotateSpeed; set => rotateSpeed = value; }
+    public Vector2 Coordinates { get => coordinates; set => coordinates = value; }
+
+    protected virtual void Move()
+    {
+        ControlSystem.Move(transform,target,MoveSpeed);
+    } 
+
+    protected virtual void Rotate()
+    {
+        ControlSystem.Rotate(transform,RotateSpeed, YRotate);
+    }
+}
